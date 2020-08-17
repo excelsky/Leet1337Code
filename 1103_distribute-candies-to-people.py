849b9a0f43d5360f7a512b8bc5d5ad6c0e313454
Kicho Yu <analytics20132333@gmail.com>
2020-08-17 11:56:04 -0700

    Create 1103_distribute-candies-to-people.py

---
+++ 1103_distribute-candies-to-people.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/distribute-candies-to-people
+# class Solution:
+#     def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+def distributeCandies(candies, num_people):
+    ans = [0 for _ in range(num_people)]
+    i = 0
+    candy = 1
+    while candies > 0:
+        if candy > candies:
+            candy = candies
+        ans[i] += candy
+        candies -= candy
+        candy += 1
+        i += 1
+        if i == len(ans):
+            i = 0
+    return ans
+
+
+assert(distributeCandies(7, 4)) == [1,2,3,1]
+assert(distributeCandies(10, 3)) == [5,2,3]
