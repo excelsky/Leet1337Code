759544f397204593796fd609b61fab195ebacf4e
Kicho Yu <analytics20132333@gmail.com>
2020-08-27 14:26:15 -0700

    Update 412_fizz-buzz.py

--- 412_fizz-buzz.py
+++ 412_fizz-buzz.py
@@ -1,28 +1,19 @@
 # https://leetcode.com/problems/fizz-buzz
-class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
+# class Solution:
+#     def fizzBuzz(self, n: int) -> List[str]:
+def fizzBuzz(n):
+    fizz_buzz_d = {3 : "Fizz", 5 : "Buzz"}
+    ans = []
+    for i in range(n):
+        fizz_buzz_s = ""
+        for key in fizz_buzz_d.keys():
+            if (i+1) % key == 0:
+                fizz_buzz_s += fizz_buzz_d[key]
+        if fizz_buzz_s == "":
+            ans.append(str(i+1))
+        else:
+            ans.append(fizz_buzz_s)
+    return ans
 
-        # ans list
-        ans = []
-
-        # Dictionary to store all fizzbuzz mappings
-        fizz_buzz_dict = {3 : "Fizz", 5 : "Buzz"}
-
-        for num in range(1,n+1):
-
-            num_ans_str = ""
-
-            for key in fizz_buzz_dict.keys():
-
-                # If the num is divisible by key,
-                # then add the corresponding string mapping to current num_ans_str
-                if num % key == 0:
-                    num_ans_str += fizz_buzz_dict[key]
-
-            if not num_ans_str:
-                num_ans_str = str(num)
-
-            # Append the current answer str to the ans list
-            ans.append(num_ans_str)  
-
-        return ans
+assert(fizzBuzz(1)) == ['1']
+assert(fizzBuzz(15)) == ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']
