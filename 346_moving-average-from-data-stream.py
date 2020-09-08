988b93acf5697def73fef1eb0404c7a575599078
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-09-08 19:08:22 +0900

    Upload 1022 and 346

---
+++ 346_moving-average-from-data-stream.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/moving-average-from-data-stream
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.size = size
+        self.queue = []
+        
+
+    def next(self, val: int) -> float:
+        size, queue = self.size, self.queue
+        queue.append(val)
+        # calculate the sum of the moving window
+        window_sum = sum(queue[-size:])
+        return window_sum / min(len(queue), size)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
