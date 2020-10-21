a7db76821d8a753cddf9129591d7db8b29cff12a
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-10-21 16:31:06 +0900

    Create 735_asteroid-collision.py

---
+++ 735_asteroid-collision.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/asteroid-collision
+# 6gaksu
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        ans = []
+        for new in asteroids:
+            while ans and new < 0 < ans[-1]:
+                if ans[-1] < -new:
+                    ans.pop()
+                    continue
+                elif ans[-1] == -new:
+                    ans.pop()
+                break
+            else:
+                ans.append(new)
+        return ans
