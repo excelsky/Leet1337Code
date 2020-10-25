609697dbc8d47949f49212cbe4993a76cf247d4e
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-10-25 16:31:41 +0900

    Update 1160_find-words-that-can-be-formed-by-characters.py

--- 1160_find-words-that-can-be-formed-by-characters.py
+++ 1160_find-words-that-can-be-formed-by-characters.py
@@ -2,11 +2,19 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         ans = 0
+
+        # Loop through words
         for word in words:
-            nope_count = 0
+            # Assign a boolean variable to check
+            # whether a letter from words is insufficient
+            # with respect to chars
+            is_letter_excessive = False
             for letter in word:
                 if word.count(letter) > chars.count(letter):
-                    nope_count += 1
-            if nope_count == 0:
+                    is_letter_excessive = True
+            # If the letter is not excessive,
+            # then we increase ans
+            if not is_letter_excessive:
                 ans += len(word)
+
         return ans
