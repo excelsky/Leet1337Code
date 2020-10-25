0d98bc24ab94a75697cde5eb4e2b6e7689d2f995
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-10-25 20:22:45 +0900

    Update 500_keyboard-row.py

--- 500_keyboard-row.py
+++ 500_keyboard-row.py
@@ -1,24 +1,27 @@
 # https://leetcode.com/problems/keyboard-row
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        row1 = "qwertyuiop"
-        row2 = "asdfghjkl"
-        row3 = "zxcvbnm"
-        
+        # List of keyboard keys per row
+        keyboard_l = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+        # Dictionary of letter and row number in the keyboard
+        letter_row_d = dict([(letter, row_num) for row_num in range(3)
+                            for letter in keyboard_l[row_num]])
+        # answer list
         ans = []
+        # Loop through words list
         for word in words:
-            method = []
-            is_same_method = 0
-            for letter_i in range(len(word)):
-                if word[letter_i].lower() in row1:
-                    method.append(1)
-                elif word[letter_i].lower() in row2:
-                    method.append(2)
-                elif word[letter_i].lower() in row3:
-                    method.append(3)
-                if letter_i > 0 and method[letter_i] != method[letter_i - 1]:
-                    is_same_method += 1
-            if is_same_method == 0:
+            row_num_l = []
+            is_same_method = True
+            for i in range(len(word)):
+                # Append method list with row number in the keyboard
+                row_num_l.append(letter_row_d[word[i].lower()])
+                # If the row number of the current key
+                # and the previous key are different,
+                # then assign is_same_method False.
+                if i > 0 and row_num_l[i] != row_num_l[i - 1]:
+                    is_same_method = False
+                    break
+            if is_same_method:
                 ans.append(word)
-                
+
         return ans
