87bd66f7b8ab5b51d9a6248fc3eb9a817ac86569
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-11-09 15:28:38 +0900

    Update 1099_two-sum-less-than-k.py

--- 1099_two-sum-less-than-k.py
+++ 1099_two-sum-less-than-k.py
@@ -1,12 +1,18 @@
 # https://leetcode.com/problems/two-sum-less-than-k
-# 6gaksu
+# class Solution:
+#     def twoSumLessThanK(self, A: List[int], K: int) -> int:
+def twoSumLessThanK(A, K):
+    A.sort()
+    left_i = 0
+    right_i = len(A) - 1
+    ans = -1
+    while left_i < right_i:
+        if A[left_i] + A[right_i] < K:
+            ans = max(ans, A[left_i] + A[right_i])
+            left_i += 1
+        else:
+            right_i -= 1
+    return ans
 
-class Solution:
-    def twoSumLessThanK(self, A: List[int], K: int) -> int:
-        S = -1
-        A.sort()
-        for i in range(len(A)):
-            j = bisect_left(A, K - A[i], i + 1) - 1
-            if j > i:
-                S = max(S, A[i] + A[j])
-        return S
+assert(twoSumLessThanK(A = [34,23,1,24,75,33,54,8], K = 60) == 58)
+assert(twoSumLessThanK(A = [10,20,30], K = 15) == -1)
