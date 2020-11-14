f399c169163e83d6843fbb638eaa1755d8a170ad
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-11-15 00:03:47 +0900

    Create 458_poor-pigs.py

---
+++ 458_poor-pigs.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/poor-pigs
+# 6gaksu
+
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        states = minutesToTest // minutesToDie + 1
+        return math.ceil(math.log(buckets) / math.log(states))
