e92b55784286ecb10591cad32b580babe3303bd0
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-11-19 23:43:43 +0900

    Update 56_merge-intervals.py

--- 56_merge-intervals.py
+++ 56_merge-intervals.py
@@ -6,15 +6,15 @@ class Solution:
 
         intervals.sort(key=lambda x: x[0])
 
-        merged = []
+        ans = []
         for interval in intervals:
             # if the list of merged intervals is empty or if the current
             # interval does not overlap with the previous, simply append it.
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
             else:
             # otherwise, there is overlap, so we merge the current and previous
             # intervals.
-                merged[-1][1] = max(merged[-1][1], interval[1])
+                ans[-1][1] = max(ans[-1][1], interval[1])
 
-        return merged
+        return ans
