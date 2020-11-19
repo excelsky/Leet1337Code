827bffd189457e4b2db72d89cef9f09c1b5f398e
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-11-20 00:10:37 +0900

    Create 394_decode-string.py

---
+++ 394_decode-string.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/decode-string
+# 6gaksu
+
+import re
+class Solution:
+    def decodeString(self, s: str) -> str:
+        while '[' in s:
+            s = re.sub(r'(\d+)\[([a-z]*)\]', lambda m: int(m.group(1)) * m.group(2), s)
+        return s
