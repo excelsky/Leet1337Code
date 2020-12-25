06c888db2fcab5e18009263747a8c9c27f72094d
Kicho 유기초 Yu <analytics20132333@gmail.com>
2020-12-25 23:54:43 +0900

    Create 24_swap-nodes-in-pairs.py

---
+++ 24_swap-nodes-in-pairs.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/swap-nodes-in-pairs/
+# 6gaksu
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Dummy node acts as the prevNode for the head node
+        # of the list and hence stores pointer to the head node.
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        prev_node = dummy
+
+        while head and head.next:
+
+            # Nodes to be swapped
+            first_node = head;
+            second_node = head.next;
+
+            # Swapping
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            # Reinitializing the head and prev_node for next swap
+            prev_node = first_node
+            head = first_node.next
+
+        # Return the new head node.
+        return dummy.next
