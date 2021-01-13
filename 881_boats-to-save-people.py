82bf3f2b9e700b0cd06af48ededc48b8e1125bc2
Kicho 유기초 Yu <analytics20132333@gmail.com>
2021-01-13 10:42:03 -0800

    Create 881_boats-to-save-people.py

---
+++ 881_boats-to-save-people.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/boats-to-save-people/
+# 6gaksu
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i, j = 0, len(people) - 1
+        ans = 0
+        while i <= j:
+            ans += 1
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+        return ans
