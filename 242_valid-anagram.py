07af70d1e3037f862c694e2b80b5649dc134a84c
Kicho 유기초 Yu <analytics20132333@gmail.com>
2021-02-11 15:53:09 -0800

    Update 242_valid-anagram.py

--- 242_valid-anagram.py
+++ 242_valid-anagram.py
@@ -1,9 +1,7 @@
 # https://leetcode.com/problems/valid-anagram
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_l = sorted(list(s))
-        t_l = sorted(list(t))
-        return s_l == t_l
+        return sorted(list(s)) == sorted(list(t))
 
 ### suboptimal solution
 '''
